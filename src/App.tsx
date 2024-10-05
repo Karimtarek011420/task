290d0c289fc3b62367c724914f6309a1504bb9e7
Karimtarek011420 <karimtarek011420@gmail.com>
2024-10-05 22:57:08 +0300

    sec commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Compoments/Layout/Layout'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const router =createBrowserRouter([
+  {path:'/',element:<Layout/> , children:[
+    {path:'/',element:<h1>Home</h1>},
+    {path:'/about',element:<h1>About</h1>},
+    {path:'/contact',element:<h1>Contact</h1>},
+
+  ]}
+])
+
+
+export default function App() {
+  return <>
+<RouterProvider router={router}/>
+  
+  </>
 }
-
-export default App;
